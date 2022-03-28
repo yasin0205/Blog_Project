10fc267dba11ee199255b0b71ac3b330d840c8af
yasin0205 <yasinislamhimaloyodesk@gmail.com.com>
2022-03-28 19:26:14 +0600

    Login and Logout

--- App_Login/views.py
+++ App_Login/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse
 
 def sign_up(request):
     form = UserCreationForm()
@@ -13,3 +18,20 @@ def sign_up(request):
     dict = {'form':form, 'registered':registered}
     return render(request, 'App_Login/signup.html', context=dict)
 
+def login_page(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None: #if user exists
+                login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+    return render(request, 'App_Login/login.html', context={'form':form})
+
+@login_required
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
